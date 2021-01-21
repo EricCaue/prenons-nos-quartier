d3225708fb6ff6b26d979b7916bb5f55f9dd8499
atcha <francois.cornuaud@gmail.com>
2021-01-21 20:24:14 +0100

    Update : Ajout icon aide sur petites images

--- src/components/DraggableImage.vue
+++ src/components/DraggableImage.vue
@@ -1,52 +1,81 @@
 <template>
-    <drag class="image"
-          effect-allowed="move"
-          drop-effect="move"
-          :transfer-data="{ id: id, dragTo: dragTo, imageName: name }">
-        <img @click="openHelpModal" :src="publicPath + 'images/' + name" alt="">
-    </drag>
+  <drag class="image"
+        effect-allowed="move"
+        drop-effect="move"
+        :transfer-data="{ id: id, dragTo: dragTo, imageName: name }">
+    <img @click="openHelpModal" :src="publicPath + 'images/' + name" alt="">
+    <div v-if="help" class="img-help" @click="openHelpModal">
+      <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
+           width="10">
+        <path fill="currentColor"
+              d="M202.021 0C122.202 0 70.503 32.703 29.914 91.026c-7.363 10.58-5.093 25.086 5.178 32.874l43.138 32.709c10.373 7.865 25.132 6.026 33.253-4.148 25.049-31.381 43.63-49.449 82.757-49.449 30.764 0 68.816 19.799 68.816 49.631 0 22.552-18.617 34.134-48.993 51.164-35.423 19.86-82.299 44.576-82.299 106.405V320c0 13.255 10.745 24 24 24h72.471c13.255 0 24-10.745 24-24v-5.773c0-42.86 125.268-44.645 125.268-160.627C377.504 66.256 286.902 0 202.021 0zM192 373.459c-38.196 0-69.271 31.075-69.271 69.271 0 38.195 31.075 69.27 69.271 69.27s69.271-31.075 69.271-69.271-31.075-69.27-69.271-69.27z"></path>
+      </svg>
+    </div>
+  </drag>
 </template>
 
 <script>
-    export default {
-        name: "DraggableImage",
-        props: ['id', 'dragTo', 'name', 'help', 'openModal'],
-        data() {
-            return {
-                publicPath: process.env.BASE_URL
-            }
-        },
-        methods: {
-            openHelpModal() {
-                this.$parent.$parent.$emit('open:modal', this.name);
-            }
-        }
+export default {
+  name: "DraggableImage",
+  props: ['id', 'dragTo', 'name', 'help', 'openModal'],
+  data() {
+    return {
+      publicPath: process.env.BASE_URL
     }
+  },
+  methods: {
+    openHelpModal() {
+      this.$parent.$parent.$emit('open:modal', this.name);
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    $width: 150px;
-    $height: 230px;
-    .image {
-        overflow: hidden;
-        display: flex;
-        margin: auto;
-        justify-content: center;
-        align-items: center;
-        width: $width * .5;
-        height: $height * .5;
-        cursor: grabbing;
-        transition: all .25s ease;
-        justify-items: center;
+$width: 150px;
+$height: 230px;
+.image {
+  z-index: 70;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+  width: $width * .5;
+  height: $height * .5;
+  cursor: grabbing;
+  transition: all .25s ease;
+  justify-items: center;
 
-        img {
-            height: 100%;
-            width: auto;
-        }
+  img {
+    height: 100%;
+    width: auto;
+  }
 
-        &:hover {
-            transform: scale(1.1);
-            border-color: #a1a1a1;
-        }
+  .img-help {
+    z-index: 80;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1.2rem;
+    height: 1.2rem;
+    bottom: .6rem;
+    right: .3rem;
+    background-color: white;
+    border-radius: 50%;
+    transition: transform .25s ease;
+
+    &:hover {
+      cursor: help;
+      transform: scale(1.2);
     }
+  }
+
+  &:hover {
+    transform: scale(1.1);
+    border-color: #a1a1a1;
+  }
+}
 </style>
